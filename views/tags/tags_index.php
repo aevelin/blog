1c056d1f2a5e4762930dfed1e530980049c3fca3
Evelin Arust <evelin.arust@khk.ee>
2018-04-01 11:49:50 +0300

    tags_index.php tags vaate loomine

--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -1,5 +1,7 @@
+
 <ul class="list-group">
+
     <?php foreach($tags as $tag): ?>
-    <a href="#"><li class="list-group-item"><?php echo $tag['tag_name']; ?></li></a>
-    <?php endforeach;; ?>
+        <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><li class="list-group-item"><?php echo $tag['tag_name']; ?></li></a>
+    <?php endforeach; ?>
 </ul>
